29d74bf96ae0db335e41b78c9300b17b78a8df2d
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-02-15 21:13:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,180 +1,84 @@
-//* Don't change this code if not destroy your files and don't steal it the code (by jonell Magallanes Project CC))
-const express = require('express');
-const fs = require('fs');
 const { spawn } = require("child_process");
-const chalk = require('chalk');
-const path = require('path');
+const { readFileSync } = require("fs-extra");
+/////////////////////////////////////////////
+//========= CHECK UPTIME =========//
+/////////////////////////////////////////////
+const http = require("http");
 const axios = require("axios");
+const semver = require("semver");
+const logger = require("./utils/log");
+const chalk = require("chalk");
+var uptimelink = [`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`]
+const Monitor = require('ping-monitor');
+for (const now of uptimelink) {
+  const monitor = new Monitor({
+    website: `${now}`,
+    title: 'Yumi',
+    interval: 59,
+  config: {
+    intervalUnits: 'seconds'
+  }
+});
+monitor.on('up', (res) => console.log(chalk.bold.hex("#00FF00")("[ Yumi ] ❯ ") + chalk.hex("#00FF00")(`${res.website}`)))
+monitor.on('down', (res) => console.log(chalk.bold.hex("#FF0000")("[ DOWN ] ❯ ") + chalk.hex("#FF0000")(`${res.website} ${res.statusMessage}`)))
+monitor.on('stop', (website) => console.log(chalk.bold.hex("#FF0000")("[ STOP ] ❯ ") + chalk.hex("#FF0000")(`${website}`)))
+monitor.on('error', (error) => console.log(chalk.bold.hex("#FF0000")("[ ERROR ] ❯ ") + chalk.hex("#FF0000")(`${error}`)))
+}
+/////////////////////////////////////////////
+//========= Check node.js version =========//
+/////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+//========= Create website for dashboard/uptime =========//
+///////////////////////////////////////////////////////////
+
+const express = require('express');
 const app = express();
-const PingMonitor = require('ping-monitor');
-const pingOptions = {
-  website: `https://symmetrical-space-disco-7qqr95rrxqrfr6gv.github.dev/pf-signin?id=puzzled-hill-0jqv5pn&cluster=asse&name=symmetrical-space-disco-7qqr95rrxqrfr6gv&port=8080&pb=https%3A%2F%2Fsymmetrical-space-disco-7qqr95rrxqrfr6gv-8080.app.github.dev%2Fauth%2Fpostback%2Ftunnel%3Ftunnel%3D1&cid=1416fadb-5ede-497b-8852-81a221ce80f4`,
-  title: 'EDUCATIONAL BOT 4.0V',
-  interval: 1 // minutes
-};
 
-// Create a new Ping Monitor instance
-const monitor = new PingMonitor(pingOptions);
+const port = process.env.PORT || 5000
+     
+app.listen(port, () =>
+	logger(`Your app is listening a http://localhost:${port}`, "[ ONLINE ]")
+     );      
 
-monitor.on('up', (res) => {
-  const pingTime = res.ping ? `${res.ping}ms` : 'N/A';
-  console.log(chalk.green.bold(`${res.website} is UP. Ping Time: ${pingTime}`));
-});
 
-monitor.on('down', (res) => {
-  console.log(chalk.red.bold(`${res.website} is DOWN. Status Message: ${res.statusMessage}`));
-});
+logger("Opened server site...", "[ Starting ]");
 
-monitor.on('error', (error) => {
-  console.log(chalk.red(`An error has occurred: ${error}`));
-});
+/////////////////////////////////////////////////////////
+//========= Create start bot and make it loop =========//
+/////////////////////////////////////////////////////////
 
-setInterval(() => {
-  if (monitor.isRunning()) {
-    console.log(chalk.green.bold('Uptime notification: The website is running smoothly.'));
-  } else {
-    console.log(chalk.red.bold('Uptime notification: The website is currently down.'));
-  }
-}, 3600000); // 60 minutes * 60 seconds * 1000 milliseconds
+function startBot(message) {
+    (message) ? logger(message, "[ Starting ]") : "";
 
-monitor.on('stop', (website) => {
-  console.log(`${website} monitor has stopped.`);
-});
-
-monitor.start();
-
-function ping(targetUrl) {
-  const startPingTime = Date.now();
-
-  axios.get(targetUrl)
-    .then(() => {
-      const latency = Date.now() - startPingTime;
-      console.log(`Ping to ${targetUrl}: ${latency}ms`);
-    })
-    .catch((error) => {
-      console.error(`Error pinging ${targetUrl}: ${error.message}`);
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "jonell.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
     });
-}
-
-// Example usage:
-setInterval(() => {
-  ping(`https://symmetrical-space-disco-7qqr95rrxqrfr6gv.github.dev/pf-signin?id=puzzled-hill-0jqv5pn&cluster=asse&name=symmetrical-space-disco-7qqr95rrxqrfr6gv&port=8080&pb=https%3A%2F%2Fsymmetrical-space-disco-7qqr95rrxqrfr6gv-8080.app.github.dev%2Fauth%2Fpostback%2Ftunnel%3Ftunnel%3D1&cid=1416fadb-5ede-497b-8852-81a221ce80f4`);
-}, 30000); 
-const config = require('./config.json'); 
-
-const commandsPath = './script/commands'; 
-const eventsPath = './script/events'; 
-
-const getFilesCount = (dirPath) => {
-  try {
-    return fs.readdirSync(dirPath).length;
-  } catch (e) {
-    return 0;
-  }
-};
-
-
-let startPingTime = Date.now();
-let botStartTime = Date.now(); 
-
-async function getBotInformation() {
-  return {
-    owner: {
-      name: config.BOTOWNER,
-      uid: config.ADMINUID,
-    },
-    bot: {
-      name: config.BOTNAME,
-      uid: config.ADMINUID,
-      fmd: config.FCA,
-      repl: config.REPL,
-      lang: config.language,
-      ping: Date.now() - startPingTime,
-      },
-    fca: {
-      module: config.FCA,
-    }
-  };
-}
-
-function sendLiveData(socket) {
-  setInterval(() => {
-    const uptime = Date.now() - botStartTime;
-
-    socket.emit('real-time-data', { uptime });
-  }, 1000); 
-}
-
-app.get('/dashboard', async (req, res) => {
-  const commandsCount = getFilesCount(commandsPath);
-  const eventsCount = getFilesCount(eventsPath);
-  const uptime = Date.now() - botStartTime;
-  const botInformation = await getBotInformation();
-
-  res.json({
-    botPing:
-     botInformation.bot.ping,
-    botLang:
-  botInformation.bot.lang,
-    botRepl:
-     botInformation.bot.repl,
-    botFmd:
-    botInformation.bot.fmd,
-    botName: botInformation.bot.name,
-    botUid: botInformation.bot.uid,
-    ownerName: botInformation.owner.name,
-    ownerUid: botInformation.owner.uid,
-    prefix: config.PREFIX,
-    commandsCount: commandsCount,
-    eventsCount: eventsCount,
-    uptime: uptime
-  });
-});
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'harold.html')));
-
-
-const http = require('http');
-const { Server } = require("socket.io");
-const httpServer = http.createServer(app);
-const io = new Server(httpServer);
-
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  sendLiveData(socket);
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
-
-function startBot() {
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "jonell.js"], {
-      cwd: __dirname,
-      stdio: "inherit",
-      shell: true
-  });
-
+  
   child.on("close", (codeExit) => {
-    console.log(`Bot process exited with code: ${codeExit}`);
-    if (codeExit !== 0) {
-       setTimeout(startBot, 3000); 
-    }
+        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+            startBot("Starting up...");
+            global.countRestart += 1;
+            return;
+        } else return;
+    });
+
+  child.on("error", function(error) {
+    logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
   });
+};
+////////////////////////////////////////////////
+//========= Check update from Github =========//
+////////////////////////////////////////////////
 
-  child.on("error", (error) => {
-    console.error(`An error occurred starting the bot: ${error}`);
-  });
-}
 
-startBot(); 
-
-const port = process.env.PORT || 8080;
-httpServer.listen(port, () => {
-  console.log(`Server with real-time updates running on http://localhost:${port}`);
+axios.get("https://raw.githubusercontent.com/Huongcq98/miraiv3/3aa476e7079e45d219697bdc4e3c2e1a8cf33314/package.json").then((res) => {
+  logger(res['data']['name'], "[ NAME ]");
+  logger("Version: " + res['data']['version'], "[ VERSION ]");
+  logger(res['data']['description'], "[ DESCRIPTION ]");
 });
-
-module.exports = app;
-
-
-//Modified by Jonell Magallanes 
+startBot();
+// THIS BOT WAS MADE BY Arjhil Dacayanan - DO NOT STEAL MY CODE.
+app.get('/', (req, res) => res.sendFile(__dirname+'/Harold.html'))
