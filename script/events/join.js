20efc885e1d536c2e196b1d230b530a7e136ecf1
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-03-02 12:08:15 +0800

    Update join.js

--- script/events/join.js
+++ script/events/join.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const axios = require("axios");
+const request = require("request");
+const moment = require("moment"); // Make sure to install 'moment' using npm install moment
+
 module.exports.config = {
     name: "join",
     eventType: ['log:subscribe'],
@@ -7,10 +12,6 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, Users, Threads }) {
-    const fs = require("fs");
-    const axios = require("axios");
-    const request = require("request");
-
     function reply(data) {
         api.sendMessage(data, event.threadID, event.messageID);
     }
@@ -32,14 +33,18 @@ module.exports.run = async function ({ api, event, Users, Threads }) {
                 nameArray = [],
                 i = 0;
             let addedParticipants1 = event.logMessageData.addedParticipants;
+            const senderID = event.senderID; // Assuming event.senderID is already defined
+
             for (let newParticipant of addedParticipants1) {
-                let userID = newParticipant.userFbId
+                let userID = newParticipant.userFbId;
                 api.getUserInfo(parseInt(userID), (err, data) => {
                     if (err) {
-                        return console.log(err)
+                        return console.log(err);
                     }
                     var obj = Object.keys(data);
+                    var firstName = data[obj].firstName;
                     var userName = data[obj].name.replace("@", "");
+
                     if (userID !== api.getCurrentUserID()) {
                         nameArray.push(userName);
                         mentions.push({
@@ -50,9 +55,9 @@ module.exports.run = async function ({ api, event, Users, Threads }) {
                         let avt = ["https://i.postimg.cc/pTGHDKnY/images-2023-08-19-T230758-444.jpg", "https://i.postimg.cc/pd0WBwwF/images-2023-08-19-T230807-555.jpg", "https://i.postimg.cc/gkvG7L9d/images-2023-08-19-T230828-578.jpg", "https://i.postimg.cc/XNfXtYyf/images-2023-08-19-T230845-301.jpg"];
                         var avt1 = avt[Math.floor(Math.random() * avt.length)];
 
-                        let firstName = nameArray[0].split(" ")[0]; // Extracting the first name from the array
+                        let firstName = nameArray[0].split(" ")[0];
 
-                        let requestURL = `https://join2apibyjonell-7b4fde8396f3.herokuapp.com/join2?name=${firstName}&id=${event.senderID}&background=${avt1}&count=${participantIDs.length}`;
+                        let requestURL = `https://join2apibyjonell-7b4fde8396f3.herokuapp.com/join2?name=${firstName}&id=${senderID}&background=${avt1}&count=${participantIDs.length}`;
 
                         let callback = function () {
                             let welcomeText = `Hello ${userName}!\nWelcome to ${tn}\nYou're the ${participantIDs.length}th member on this group. Enjoy!`;
